4bc4c42069713cdbdaff7526e6421c2a2c206f9c
David Dulce <davidulce26@gmail.com>
2023-08-23 22:31:07 -0500

    add model

---
+++ api/src/main/java/dev/dvrgnx/movies/model/Review.java
@@ -0,0 +1,31 @@
+package dev.dvrgnx.movies.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "reviews")
+public class Review {
+
+    private ObjectId id;
+
+    private String body;
+
+    private LocalDateTime created;
+
+    private LocalDateTime updated;
+
+    public Review(String body, LocalDateTime created, LocalDateTime updated) {
+        this.body = body;
+        this.created = created;
+        this.updated = updated;
+    }
+
+}
